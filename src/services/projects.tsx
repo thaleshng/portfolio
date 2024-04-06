ad28b8e1dd80695efab661ecaf88f09ca29deb9d
Thales Gomes <thaleshng@gmail.com>
2024-04-06 16:56:16 -0300

    testando diretório

--- src/services/projects.tsx
+++ src/services/projects.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function fetchProjects() {
     try {
-        const response = await axios.get("./portfolio/public/projects.json")
+        const response = await axios.get("../../public/abilities.json")
         return response.data
     } catch (error) {
         console.error("Erro ao buscar o arquivo JSON:", error);
